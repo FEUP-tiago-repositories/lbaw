669bcaede6f6e412080e24cae82acd240f4ac272
tiagoanoliveira <up202007448@up.pt>
2025-11-05 21:10:06 +0000

    Fix and update sql

--- artifacts/EBD/schema.sql
+++ artifacts/EBD/schema.sql
@@ -4,7 +4,7 @@ create schema lbaw25122;
 
 set search_path to lbaw25122;
 
-DROP Table IF EXISTS "users" CASCADE;
+DROP Table IF EXISTS "user" CASCADE;
 
 DROP TABLE IF EXISTS "business_owner" CASCADE;
 
@@ -32,21 +32,23 @@ DROP TABLE IF EXISTS "discount" CASCADE;
 
 DROP TABLE IF EXISTS "notification" CASCADE;
 
+DROP TABLE IF EXISTS "response_notification" CASCADE;
+
 DROP TABLE IF EXISTS "review_notification" CASCADE;
 
 DROP TABLE IF EXISTS "booking_confirmation_notification" CASCADE;
 
-DROP TABLE IF EXISTS "booking_cancelation_notification" CASCADE;
+DROP TABLE IF EXISTS "booking_cancellation_notification" CASCADE;
 
 DROP TABLE IF EXISTS "booking_reminder_notification" CASCADE;
 
 DROP TABLE IF EXISTS "media" CASCADE;
 
 DROP TABLE IF EXISTS "favorited" CASCADE;
---not to reapeat any table
+--not to repeat any table
 
-CREATE Table users (
-    user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE Table "user" (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_name VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     phone_no VARCHAR(15) NOT NULL UNIQUE,
@@ -60,17 +62,17 @@ CREATE Table users (
 );
 
 CREATE TABLE business_owner (
-    business_owner_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE
 );
 
 CREATE TABLE customer (
-    customer_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE
 );
 
 CREATE TABLE sport_type (
-    sport_type_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50) NOT NULL UNIQUE CHECK (
         name IN (
             'Badminton',
@@ -96,9 +98,9 @@ CREATE TABLE sport_type (
 );
 
 CREATE TABLE space(
-    space_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    owner_id INT NOT NULL REFERENCES business_owner (business_owner_id) ON DELETE RESTRICT,
-    sport_type_id INT NOT NULL REFERENCES sport_type (sport_type_id) ON DELETE RESTRICT,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    owner_id INT NOT NULL REFERENCES business_owner (id) ON DELETE RESTRICT,
+    sport_type_id INT NOT NULL REFERENCES sport_type (id) ON DELETE RESTRICT,
     title VARCHAR(100) NOT NULL,
     address VARCHAR(150) NOT NULL,
     description VARCHAR(300) NOT NULL,
@@ -110,22 +112,22 @@ CREATE TABLE space(
 );
 
 CREATE TABLE admin (
-    admin_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE ban (
-    ban_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id INT NOT NULL REFERENCES users (user_id),
-    admin_id INT NOT NULL REFERENCES admin (admin_id) ON DELETE CASCADE,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL REFERENCES "user" (id),
+    admin_id INT NOT NULL REFERENCES admin (id) ON DELETE CASCADE,
     motive VARCHAR(200) NOT NULL,
     time_stamp TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE schedule (
-    schedule_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    space_id INT NOT NULL REFERENCES space(space_id),
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    space_id INT NOT NULL REFERENCES space (id),
     schedule_date DATE NOT NULL CHECK (schedule_date > NOW()), --must be a future DATE
     start_time TIMESTAMP NOT NULL CHECK (start_time > NOW()), --must be a future TIMESTAMP
     duration INT NOT NULL CHECK (duration > 0),
@@ -133,18 +135,18 @@ CREATE TABLE schedule (
 );
 
 CREATE TABLE booking (
-    booking_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    space_id INT NOT NULL REFERENCES space(space_id),
-    customer_id INT NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
-    schedule_id INT NOT NULL REFERENCES schedule (schedule_id) ON DELETE CASCADE,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    space_id INT NOT NULL REFERENCES space (id),
+    customer_id INT NOT NULL REFERENCES customer (id) ON DELETE CASCADE,
+    schedule_id INT NOT NULL REFERENCES booking (id) ON DELETE CASCADE,
     booking_created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     is_cancelled BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE review (
-    review_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    customer_id INT REFERENCES customer (customer_id) ON DELETE SET NULL,
-    booking_id INT REFERENCES booking (booking_id) ON DELETE SET NULL,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    customer_id INT REFERENCES customer (id) ON DELETE SET NULL,
+    booking_id INT REFERENCES booking (id) ON DELETE SET NULL,
     time_stamp TIMESTAMP NOT NULL DEFAULT NOW(),
     text VARCHAR(500) NOT NULL,
     environment_rating SMALLINT NOT NULL CHECK (
@@ -159,79 +161,85 @@ CREATE TABLE review (
 );
 
 CREATE TABLE response (
-    response_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    owner_id INT REFERENCES business_owner (business_owner_id) ON DELETE SET NULL,
-    review_id INT NOT NULL REFERENCES review (review_id) ON DELETE CASCADE,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    owner_id INT REFERENCES business_owner (id) ON DELETE SET NULL,
+    review_id INT NOT NULL REFERENCES review (id) ON DELETE CASCADE,
     text VARCHAR(300) NOT NULL,
     time_stamp TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE payment (
-    payment_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    booking_id INT REFERENCES booking (booking_id) ON DELETE SET NULL,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    booking_id INT REFERENCES booking (id) ON DELETE SET NULL,
     value FLOAT NOT NULL CHECK (value > 0),
     is_discounted BOOLEAN NOT NULL DEFAULT FALSE,
     is_accepted BOOLEAN NOT NULL DEFAULT FALSE,
     payment_provider_ref VARCHAR(100) NOT NULL CHECK(
         payment_provider_ref IN (
-            "Credit/Debit Card",
-            "MB Way",
-            "Paypal"
+            'Credit/Debit Card',
+            'MB Way',
+            'Paypal'
         )
     ),
     time_stamp TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE discount (
-    discount_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    space_id INT NOT NULL REFERENCES space(space_id),
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    space_id INT NOT NULL REFERENCES space (id),
     percentage FLOAT NOT NULL CHECK (percentage BETWEEN 0 AND 100),
     start_date TIMESTAMP NOT NULL,
     end_date TIMESTAMP NOT NULL CHECK (end_date > start_date)
 );
 
 CREATE TABLE notification (
-    notification_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
     time_stamp TIMESTAMP NOT NULL DEFAULT NOW(),
     is_read BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+CREATE TABLE response_notification (
+     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     notification_id INT NOT NULL REFERENCES notification (id) ON DELETE CASCADE, -- Links to notification table
+     response_id INT NOT NULL REFERENCES response (id) ON DELETE CASCADE -- Links to response table
+);
+
 CREATE TABLE review_notification (
-    review_notification_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    notification_id INT NOT NULL REFERENCES notification (notification_id) ON DELETE CASCADE, -- Links to notification table
-    review_id INT NOT NULL REFERENCES review (review_id) ON DELETE CASCADE -- Links to review table
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    notification_id INT NOT NULL REFERENCES notification (id) ON DELETE CASCADE, -- Links to notification table
+    review_id INT NOT NULL REFERENCES review (id) ON DELETE CASCADE -- Links to review table
 );
 
 CREATE TABLE booking_confirmation_notification (
-    booking_confirmation_notification_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    notification_id INT NOT NULL REFERENCES notification (notification_id) ON DELETE CASCADE, -- Links to notification table
-    booking_id INT NOT NULL REFERENCES booking (booking_id) ON DELETE CASCADE -- Links to booking table
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    notification_id INT NOT NULL REFERENCES notification (id) ON DELETE CASCADE, -- Links to notification table
+    booking_id INT NOT NULL REFERENCES booking (id) ON DELETE CASCADE -- Links to booking table
 );
 
-CREATE TABLE booking_cancelation_notification (
-    booking_cancelation_notification_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    notification_id INT NOT NULL REFERENCES notification (notification_id) ON DELETE CASCADE, -- Links to notification table
-    booking_id INT NOT NULL REFERENCES booking (booking_id) ON DELETE CASCADE -- Links to booking table
+CREATE TABLE booking_cancellation_notification (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    notification_id INT NOT NULL REFERENCES notification (id) ON DELETE CASCADE, -- Links to notification table
+    booking_id INT NOT NULL REFERENCES booking (id) ON DELETE CASCADE -- Links to booking table
 );
 
 CREATE TABLE booking_reminder_notification (
-    booking_reminder_notification_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    notification_id INT NOT NULL REFERENCES notification (notification_id) ON DELETE CASCADE, -- Links to notification table
-    booking_id INT NOT NULL REFERENCES booking (booking_id) ON DELETE CASCADE -- Links to booking table
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    notification_id INT NOT NULL REFERENCES notification (id) ON DELETE CASCADE, -- Links to notification table
+    booking_id INT NOT NULL REFERENCES booking (id) ON DELETE CASCADE -- Links to booking table
 );
 
 CREATE TABLE media (
-    media_id INT GENERATED ALWAYS AS IDENTITY,
-    space_id INT NOT NULL REFERENCES space(space_id) ON DELETE CASCADE,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    space_id INT NOT NULL REFERENCES space (id) ON DELETE CASCADE,
     media_url VARCHAR(255) NOT NULL,
     is_cover BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (media_id, space_id)
+    PRIMARY KEY (id, space_id)
 );
 
 CREATE TABLE favorited (
-    space_id INT NOT NULL REFERENCES space(space_id) ON DELETE CASCADE,
-    customer_id INT NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
+    space_id INT NOT NULL REFERENCES space (id) ON DELETE CASCADE,
+    customer_id INT NOT NULL REFERENCES customer (id) ON DELETE CASCADE,
     is_favorite BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (space_id, customer_id)
 );

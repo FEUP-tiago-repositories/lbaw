bfd837d7207090bfbabd0c8acdf90e6cc30bf476
tiagoleic02 <up202007448@up.pt>
2025-11-21 00:26:37 +0000

    aDD

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,7 @@
+<head>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-blue-500 text-white text-center">
+    <h1 class="text-3xl text-blue-600 font-bold underline">Tailwind OK</h1>
+</body>

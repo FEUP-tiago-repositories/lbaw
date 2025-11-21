1402be73a6938dfb759dc10bf7681bfd9ecfef8c
Gustavo Lourenço <up202306578@up.pt>
2025-11-21 11:42:46 +0000

    Completed Tailwind setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'SportsHub') }}</title>
-    
+
     {{-- CSS --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
+
 <body>
     <nav class="navbar">
         {{-- Navegação --}}
         <a href="{{ route('home') }}">Home</a>
         <a href="{{ route('spaces.index') }}">Espaços</a>
-    
+
     </nav>
 
     <main>
@@ -23,4 +26,5 @@
     {{-- JavaScript --}}
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
+
 </html>

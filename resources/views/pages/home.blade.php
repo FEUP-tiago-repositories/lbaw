e3878c3a466c8bcb1dea51dac63c4a3807ee3694
tiagoanoliveira <up202007448@up.pt>
2025-11-21 22:45:09 +0000

    Fix: Add image from beach to main page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class = "pt-96 text-center">
+<div class = "pt-56 text-center">
     <h1>Bem-vindo ao SportsHub</h1>
     <p>Encontre e reserve os melhores espaços desportivos.</p>
 
     {{-- Adicione aqui o conteúdo da homepage --}}
     <div class="featured-spaces pt-8">
         <h2>Enquanto que o website não se encontra pronto, fique sentado a apreciar esta praia ⮧</h2>
-        <img src="../../../public/images/praia.jpg" alt="🏖️">
+        <img class="mx-auto block pt-4" src="/images/praia.jpg" alt="🏖️">
         {{-- Lista de espaços será adicionada aqui --}}
     </div>
 </div>

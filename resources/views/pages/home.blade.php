e390de35c4962941bb15646166cafb2c30dffc9d
Tiago Zou Yin <up202306438@edu.fe.up.pt>
2025-11-22 23:53:17 +0000

    feat(homepage): add list of best reviewed spaces

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,12 +4,14 @@
 <div class = "pt-56 text-center">
     <img class="mx-auto block pt-4" src="/images/praia.jpg" alt="🏖️">
     <h2>Best Reviewed:</h2>
-    @foreach ($spaces as $space)
-        <div class="featured-spaces pt-8">
-            <h3 class="space name">{{ $space->title }}</h3>
-            <p class="space address">{{ $space->address }}</p>
-            <p class="space num_favorites">Favorites: {{ $space->num_favorites }}</p>
-        </div>
-    @endforeach
+    <div class="featured-spaces pt-8">
+        @foreach ($spaces as $space)
+            <div class="space-card">
+                <h3 class="space name">{{ $space->title }}</h3>
+                <p class="space address">{{ $space->address }}</p>
+                <p class="space num_favorites">Favorites: {{ $space->num_favorites }}</p>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection

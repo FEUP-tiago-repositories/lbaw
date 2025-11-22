6d46c86ff2b52735ae5072c47679072ae1a83c5b
Gustavo Lourenço <up202306578@up.pt>
2025-11-22 15:48:45 +0000

    feat: Created the initial Model for Spaces, now with attributes

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
 {
-    //
+    public $timestamps = false;
+
+    // our table is named Space, so we execute this command:
+    protected $table = 'space';
+
+    protected $fillable = [
+        'title',
+        'address',
+        'description',
+        'is_closed',
+        'phone_no',
+        'email',
+    ];
+
+    protected $primaryKey = 'id';
 }

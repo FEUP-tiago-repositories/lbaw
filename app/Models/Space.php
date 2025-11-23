67f482f5bbcd6bd201fa6aa8546b3aeceba8bbb4
Gustavo Lourenço <up202306578@up.pt>
2025-11-23 14:31:06 +0000

    Created Model for Business Owner and Customer

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
-{
+{   
+    //space is associated with one business owner
     public $timestamps = false;
 
     // our table is named Space, so we execute this command:

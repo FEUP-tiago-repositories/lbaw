e0df75e1d3d598c217f094557ae850314edf4dd3
tiagoanoliveira <up202007448@up.pt>
2025-12-22 17:25:24 +0000

    Update image in browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     @endauth
 
     <title>@yield('title', 'Sports Hub')</title>
-
+    <link rel="shortcut icon" href="./images/logo.svg">
     {{-- Leaflet CSS --}}
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
